23da55b828a6f52dfa45a6a918a0ce21c639b04d
Tomas Kelemen (vudiq) <tomas.kelemen@gmail.com>
2018-02-20 20:21:50 +0100

    libretro update - build job 2018-02-20

--- packages/libretro/beetle-pce/package.mk
+++ packages/libretro/beetle-pce/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-pce"
-PKG_VERSION="d3a1520"
+PKG_VERSION="f525439"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
--- packages/libretro/prboom/package.mk
+++ packages/libretro/prboom/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="prboom"
-PKG_VERSION="a907bcc"
+PKG_VERSION="76261fb"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
