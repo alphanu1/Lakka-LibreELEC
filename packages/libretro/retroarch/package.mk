5b8f4502692560c38146f24f75d3b22e383baad0
Tomas Kelemen (vudiq) <tomas.kelemen@gmail.com>
2018-02-21 23:07:36 +0100

    RetroArch: 1.7.1 release

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="cf7785d"
+PKG_VERSION="6fc6bfb"
 PKG_REV="9"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
