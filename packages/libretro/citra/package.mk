b55c03a421ff463505be6a7d74e425a55851d878
Tomas Kelemen (vudiq) <tomas.kelemen@gmail.com>
2018-03-07 09:31:20 +0100

    citra: revert to last buildable commit

--- packages/libretro/citra/package.mk
+++ packages/libretro/citra/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="citra"
-PKG_VERSION="9b6f6a9"
+PKG_VERSION="c56cff5"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2+"
