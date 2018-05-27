48d68a693dfc7981c9a51f14c6fe4dcbbd230ccc
Tomas Kelemen (vudiq) <tomas.kelemen@gmail.com>
2018-05-27 12:08:14 +0200

    libretro update - build job 2018-05-27

--- packages/libretro/mame2003-plus/package.mk
+++ packages/libretro/mame2003-plus/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mame2003-plus"
-PKG_VERSION="fe807ef"
+PKG_VERSION="3c39594"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
--- packages/libretro/snes9x/package.mk
+++ packages/libretro/snes9x/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="snes9x"
-PKG_VERSION="4f5466f"
+PKG_VERSION="ac4b54c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
