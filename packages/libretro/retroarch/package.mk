8c6681b58e01ed11a13ac0f0accb18e182bf540b
Tomas Kelemen (vudiq) <tomas.kelemen@gmail.com>
2018-06-02 12:08:20 +0200

    libretro update - build job 2018-06-02

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="861d872"
+PKG_VERSION="e4476ec"
 PKG_REV="11"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
