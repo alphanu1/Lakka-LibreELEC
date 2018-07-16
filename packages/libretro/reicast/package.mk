f4017b63e1cd643f4a5786efb660d80eb0e3dcfd
Tomas Kelemen (vudiq) <tomas.kelemen@gmail.com>
2018-07-16 12:08:18 +0200

    libretro update - build job 2018-07-16

--- packages/libretro/reicast/package.mk
+++ packages/libretro/reicast/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="reicast"
-PKG_VERSION="30e1a64"
+PKG_VERSION="fec826e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="311b454"
+PKG_VERSION="b878a94"
 PKG_REV="11"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
