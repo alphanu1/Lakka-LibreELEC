f44b767d29314434072308696ea50c1e4e3191b3
Tomas Kelemen (vudiq) <tomas.kelemen@gmail.com>
2018-07-29 12:08:22 +0200

    libretro update - build job 2018-07-29

--- packages/libretro/mame2003-plus/package.mk
+++ packages/libretro/mame2003-plus/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mame2003-plus"
-PKG_VERSION="fe84ef0"
+PKG_VERSION="ba862e0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="20335cd"
+PKG_VERSION="2faa250"
 PKG_REV="11"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
